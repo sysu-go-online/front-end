c74ab05992c79ec9802bad47cc7867fefb68904a
Dennis <937889621@qq.com>
2018-11-05 16:47:49 +0800

    Update editor.vue

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="editor">
     <div id="function-area">
-      <span v-if="file != ''" class="file">
-        <span class="file-name">{{file}}</span>
-        <span class="span_icon">
-          <svg class="icon icon-search-close" @click="closeFile"><use xlink:href="#icon-search-close"></use></svg>
-       </span>
+      <!-- 标签栏 -->
+      <!-- TODO:
+      1. 改成多标签
+      2. 识别脏状态
+      3. 文件缩略图-->
+      <span v-if="this.currentFileID != ''">
+        <span v-for="fileid in this.fileIDs" :key=fileid :class="['tab', {active: files[fileid].isSelected}]" @click="switchFile(fileid)">
+          <span class="file-name">{{ files[fileid].name }}</span>
+          <!-- TODO:
+          1. 同名时显示路径
+          2. 显示脏状态-->
+          <span class="span_icon">
+            <svg class="icon icon-search-close" @click.stop="checkDirtyThenClose(fileid)"><use xlink:href="#icon-search-close"></use></svg>
+          </span>
+        </span>
       </span>
       <span id="save"><input type="button" value="保存" id="save-button" @click="Save"></span>
     </div>
-    <div id="edit-area" @keyup.ctrl.83.stop="Save" @keydown.ctrl.83.prevent>
+    <div id="edit-area" @keyup.ctrl.83.stop="Save(currentFileID)" @keydown.ctrl.83.prevent>
+      <!-- 编辑区 -->
+      <!-- TODO: 本地缓存 -->
       <MonacoEditor
         class="editor"
-        v-model="code"
+        v-model="currentCode"
         language="go"
         theme="vs-dark"
         ref="editor"
@@ -28,7 +40,11 @@ export default {
   name: 'Editor',
   data () {
     return {
-      code: '',
+      currentCode: '',
+      // id作为key
+      openedCodes: new Map(),
+      // 同fileID，用来存储打开代码的顺序
+      fileIDs: [],
       cmOptions: {
         // codemirror options
         indentUnit: 2,
@@ -41,13 +57,23 @@ export default {
         line: true,
         language: 'go'
       },
-      file: '',
-      filedata: this.fileData,
-      filepath: ''
+      // 已打开文件的信息
+      files: {},
+      currentFileID: ''
     };
   },
   props: {
-    fileData: {
+    currentFile: {
+      default: function () {
+        return {};
+      }
+    },
+    toDeleteFileId: {
+      default: function () {
+        return {};
+      }
+    },
+    toRenameFile: {
       default: function () {
         return {};
       }
@@ -59,7 +85,7 @@ export default {
   },
   methods: {
     updateFunc (newCode, evt) {
-      this.code = newCode;
+      this.currentCode = newCode;
     },
     onCmReady (cm) {
 
@@ -68,45 +94,155 @@ export default {
 
     },
     onCmCodeChange (newCode) {
-      this.code = newCode;
+      this.currentCode = newCode;
     },
-    Save () {
-      let that = this;
-      this.$http.patch('/api/users/' + this.$cookie.get('username') + '/projects/' + this.projectName + '/files/' + this.filepath, {
+    // OK:
+    Save (fileid) {
+      let file = this.files[fileid];
+      if (file.isSelected) {
+        this.openedCodes.set(fileid, this.currentCode);
+      }
+      // console.log("current code ", this.currentCode);
+      // console.log("save code", this.openedCodes.get(fileid));
+      this.$http.patch('/api/users/' + this.$cookie.get('username') + '/projects/' + this.projectName + '/files/' + file.path, {
         'operation': 'update',
-        'content': this.code
+        'content': this.openedCodes.get(fileid)
       }, {
         headers: {'Authorization': this.$cookie.get('jwt')}
       }).then(Response => {
-        that.$dialog.alert('保存成功');
+        // TODO：修改为气泡提示格式
+        if (this.files[fileid]) {
+          this.files[fileid].isDirty = false;
+        }
+        this.$dialog.alert('保存成功', {backdropClose: true});
+      }).catch(() => {
+        this.$dialog.alert('保存失败', {backdropClose: true});
       });
     },
-    getFile: function (val, that) {
-      if (val.name === '') {
-        that.code = '';
+    getFile: function (file) {
+      // console.log(file);
+      if (file.name === '') {
+        // 貌似不可能存在？
+        this.currentCode = '';
+        // console.log('invalid name');
+        return;
+      } else if (this.openedCodes.has(file.id)) {
+        this.currentCode = this.openedCodes.get(file.id);
+        // console.log('got from log ', this.currentCode);
         return;
       }
-      this.filepath = val.filepath;
-      that.$http.get('/api/users/' + this.$cookie.get('username') + '/projects/' + this.projectName + '/files/' + val.filepath, {
-        headers: {'Authorization': that.$cookie.get('jwt')}
+      this.$http.get('/api/users/' + this.$cookie.get('username') + '/projects/' + this.projectName + '/files/' + file.path, {
+        headers: {'Authorization': this.$cookie.get('jwt')}
       }).then(Response => {
-        that.code = Response.data.toString();
-        // if (that.filepath.match(/.cpp$/)) {
-        //   that.language = 'cpp';
-        //   that.$refs.editor.$refs.editor.getMonaco().layout();
-        // }
+        // console.log(Response);
+        this.currentCode = Response.data.toString();
+        this.openedCodes.set(file.id, this.currentCode);
+        this.fileIDs.push(file.id);
       });
     },
-    closeFile: function () {}
+    switchFile: function (fileid) {
+      if (fileid === this.currentFileID) return;
+      // 本地保存当前代码
+      // TODO-BUG: 输入完代码后立即单击切换文件，有时会无法保存当前文件代码
+      if (this.currentFileID) {
+        this.openedCodes.set(this.currentFileID, this.currentCode);
+      }
+      // 切换当前文件
+      this.files[fileid].isSelected = true;
+      this.$emit('openfile', this.files[fileid], this.projectName);
+      // console.log("changed");
+    },
+    checkDirtyThenClose: function (fileid) {
+      let file = this.files[fileid];
+      // console.log(file);
+      if (file.isDirty) {
+        // 提示是否保存
+        this.$dialog.confirm('是否要保存对 ' + this.files[fileid].name + ' 的更改？', {
+          okText: '是',
+          cancelText: '否',
+          backdropClose: true
+        }).then(() => {
+        // 是
+          this.Save(fileid);
+          this.closeFile(fileid);
+        }).catch(() => {
+          this.closeFile(fileid);
+        });
+      } else {
+        this.closeFile(fileid);
+      }
+    },
+    closeFile: function (fileid) {
+      let file = this.files[fileid];
+      let indexOfID = this.fileIDs.indexOf(fileid);
+      this.fileIDs.splice(indexOfID, 1);
+      // console.log(file);
+      if (file.isSelected) {
+        // 顺延打开下一文件
+        if (this.fileIDs[indexOfID]) {
+          this.switchFile(this.fileIDs[indexOfID]);
+        } else if (this.fileIDs[--indexOfID]) { // 若不存在，则顺延打开上一文件
+          this.switchFile(this.fileIDs[indexOfID]);
+        } else { // 若不存在，则文件队列为空，清楚当前文件信息
+          this.currentCode = '';
+          this.currentFileID = '';
+          this.$emit('nofileopen');
+        }
+      }
+      // 删除文件记录及代码记录
+      // FIXED-BUG:已删除，但不更新页面
+      delete this.files[fileid];
+      // console.log("deleted");
+      this.openedCodes.delete(fileid);
+      // TODO: 待改良
+      this.$emit('closeFile', fileid);
+      this.$forceUpdate();
+    }
   },
   watch: {
-    fileData: {
-      handler: function (val, oldVal) {
-        // console.log(JSON.stringify(val));
-        this.filedata = val;
-        this.file = val.name;
-        this.filepath = val.filepath;
-        this.getFile(val, this);
+    currentFile: {
+      handler: function (file, oldFile) {
+        if (file === undefined) return;
+        // 失活上一标签
+        if (JSON.stringify(oldFile) !== '{}' && oldFile !== undefined) {
+          if (oldFile.id === file.id) {
+            // 待测试：同步重命名
+            if (oldFile.name !== file.name) {
+              this.files[file.id].name = file.name;
+            }
+            return;
+          }
+          // 切换文件，更新上一文件代码缓存
+          if (this.files[oldFile.id]) {
+            this.files[oldFile.id].isSelected = false;
+            this.openedCodes.set(oldFile.id, this.currentCode);
+          }
+        }
+        this.currentFileID = file.id;
+        this.files[this.currentFileID] = file;
+        this.getFile(file);
+      },
+      deep: true
+    },
+    currentCode: function (cCode) {
+      var file = this.files[this.currentFileID];
+      if (!file.isDirty) {
+        this.$http.get('/api/users/' + this.$cookie.get('username') + '/projects/' + this.projectName + '/files/' + file.path, {
+          headers: {'Authorization': this.$cookie.get('jwt')}
+        }).then(Response => {
+          // console.log(Response);
+          file.isDirty = Response.data.toString() !== cCode;
+        });
+      }
+    },
+    toDeleteFileId: function (id, oldId) {
+      // this.currentFileID = null;
+      this.closeFile(id);
+    },
+    toRenameFile: {
+      handler: function (file, oldFile) {
+        this.files[file.id].name = file.name;
+        this.files[file.id].path = file.path;
       },
       deep: true
     }
@@ -116,7 +252,7 @@ export default {
 </script>
 
 <style>
-.editor{
+.editor {
     position: relative;
     height: 100%;
     width: 100%;
@@ -125,7 +261,7 @@ export default {
     display: inline-block;
     overflow: hidden;
 }
-#edit-area{
+#edit-area {
     position:absolute;
     margin:0;
     height: calc(100% - 32px);
@@ -150,7 +286,7 @@ export default {
   height: 9px;
   color: rgba(219, 218, 218, 0.4)
 }
-.func_button{
+.func_button {
     background: rgba(17, 17, 17, 0.4);
     color: #009A61;
     margin: 5px;
@@ -161,23 +297,35 @@ export default {
     width: 70px;
     text-align: center;
 }
-.file {
+/* TODO：修改标签栏样式 */
+.active {
+   background-color: #282C34 !important;
+}
+
+.tab {
   font-size: 16px;
   color: #009A61;
   display: inline-block;
   height: 100%;
+  min-width: 200px;
   padding: 3px 10px;
-  border: solid 1px;
-  background: #2b2b2b;
-  border-color: #2b2b2b  #bebebe #009A61  #bebebe;
+  border: 1px solid #4e5653;
+  border-left: 0px;
+  background:  rgba(17, 17, 17, 0.4);
+  /* border-color: #2b2b2b  #bebebe #009A61  #bebebe; */
 }
-.file .span_icon {
+.tab .span_icon {
+  float: right;
   visibility: hidden;
-  display: inline-block;
+  display: block;
 }
-.file:hover .span_icon {
+.tab:hover {
+  background-color: #323842 !important;
+  cursor: pointer;
+}
+.tab:hover .span_icon {
   visibility: visible;
-  display: inline-block;
+  /* display: inline-block; */
 }
 #save {
   display: inline-block;
