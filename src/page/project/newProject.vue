d923c60751f59e09a213d3bcf9a01787a518a26f
HeartUnderBlade <pfjhyyj@qq.com>
2018-11-05 11:50:01 +0800

    add path to project create page

--- src/page/project/newProject.vue
+++ src/page/project/newProject.vue
@@ -11,10 +11,13 @@
               <mu-text-field v-model="form.description" prop="description"></mu-text-field>
             </mu-form-item>
             <mu-flex align-items="center" style="padding-bottom: 8px;">
-              <span style="margin-right: 16px;">运行环境</span>
-              <mu-radio v-model="form.language" class="radio" value="0" label="Go"></mu-radio>
-              <mu-radio v-model="form.language" class="radio" value="1" label="C"></mu-radio>
+              <span style="margin-right: 16px;">从远程仓库拷贝代码</span>
+              <mu-radio v-model="form.isClone" class="radio" value="true" label="是"></mu-radio>
+              <mu-radio v-model="form.isClone" class="radio" value="" label="否"></mu-radio>
             </mu-flex>
+            <mu-form-item prop="path" label="Remote Path" :rules="pathRules" v-if="form.isClone">
+              <mu-text-field v-model="form.path" prop="path"></mu-text-field>
+            </mu-form-item>
             <mu-form-item prop="git_path" label="Git Path" :rules="gitpathRules">
               <mu-text-field v-model="form.git_path" prop="git_path"></mu-text-field>
             </mu-form-item>
@@ -34,18 +37,6 @@ export default {
   name: 'AddProject',
   data () {
     return {
-      languages: [
-        'go',
-        'c'
-      ],
-      // c_versions: [
-      //   'c98',
-      //   'c11'
-      // ],
-      // go_versions: [
-      //   '1.9.0'
-      // ],
-      // versions: [],
       nameRules: [
         { validate: (val) => !!val, message: '必须填写项目名' },
         { validate: (val) => val.length <= 20, message: '项目名长度小于20' }
@@ -53,16 +44,20 @@ export default {
       descriptionRules: [
         { validate: (val) => val.length <= 50, message: '描述长度小于50' }
       ],
+      pathRules: [
+        { validate: (val) => !!val, message: '必须填写远程路径' },
+        { validate: (val) => val.length <= 40, message: '地址长度小于40' }
+      ],
       gitpathRules: [
         { validate: (val) => !!val, message: '必须填写git地址' },
         { validate: (val) => val.length <= 40, message: '地址长度小于40' }
       ],
       form: {
         name: '',
-        language: '',
         description: '',
+        isClone: '',
+        path: '',
         git_path: ''
-        // version: '',
       }
     };
   },
@@ -74,14 +69,14 @@ export default {
       let that = this;
       this.$refs.form.validate().then((result) => {
         if (!result) return;
+        var isClone = false;
+        if (this.form.isClone) isClone = true;
         this.$http.post('/api/users/' + this.$cookie.get('username') + '/projects', {
           'name': this.form.name,
           'description': this.form.description,
-          'language': parseInt(this.form.language),
+          'path': this.form.path,
           'git_path': this.form.git_path,
-          'setting': {
-            'username': this.$cookie.get('username')
-          }
+          'isClone': isClone
         }, {
           headers: {'Authorization': this.$cookie.get('jwt')}
         }).then(function (response) {
