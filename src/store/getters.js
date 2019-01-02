b9e29592a75b8e30a8519eafd7b7ecdbb56f28f1
Dennis <937889621@qq.com>
2019-01-02 10:17:41 +0800

    restructure

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,4 @@
 export const projectName = state => state.projectName;
-export const tree = state => state.tree;
-export const currentFile = state => state.currentFile;
-export const openFileOrder = state => state.openFileOrder;
+export const currentFilepath = state => state.currentFilepath;
+export const openFilepathOrder = state => state.openFilepathOrder;
 export const openFiles = state => state.openFiles;
