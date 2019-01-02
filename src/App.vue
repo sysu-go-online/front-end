4666cf346626fb37fdbc92e76b1dc6ec5f2591bf
Dennis <937889621@qq.com>
2019-01-02 10:18:48 +0800

    change font

--- src/App.vue
+++ src/App.vue
@@ -20,17 +20,17 @@ export default {
 </script>
 
 <style>
-html, body{
+html, body {
     height: 100%;
     width: 100%;
     margin: 0;
     padding: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #385c80;
+  color: #C2C2C3;
   height: 100%;
   width: 100%;
 }
