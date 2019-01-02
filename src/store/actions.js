96847e9992eb5e8b22aab657b07b5a535512cec1
Dennis <937889621@qq.com>
2019-01-02 10:18:06 +0800

    restructure

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,11 @@
 export const setProjectName = ({commit}, payload) => {
   commit('setProjectName', payload);
 };
-export const setTree = ({commit}, payload) => {
-  commit('setTree', payload);
+export const setCurrentFilepath = ({commit}, payload) => {
+  commit('setCurrentFilepath', payload);
 };
-export const setCurrentFile = ({commit}, payload) => {
-  commit('setCurrentFile', payload);
-};
-export const setOpenFileOrder = ({commit}, payload) => {
-  commit('setOpenFileOrder', payload);
+export const setOpenFilepathOrder = ({commit}, payload) => {
+  commit('setOpenFilepathOrder', payload);
 };
 export const setOpenFiles = ({commit}, payload) => {
   commit('setOpenFiles', payload);
